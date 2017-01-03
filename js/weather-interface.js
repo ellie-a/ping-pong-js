7ade7b37639484308975c3a95eab3f6ec2847668
Ellie Angell and Ezra Sandzer-Bell <ellie+ezra@>
2017-01-02 16:23:50 -0800

    not working

---
+++ js/weather-interface.js
@@ -0,0 +1,12 @@
+var apiKey = "1fe9e087b202c21b52c3f82a082af452";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
