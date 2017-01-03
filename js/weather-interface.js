abf8db958834bbcc2e59e362a04ed5295afc39e8
Ellie Angell and Ezra Sandzer-Bell <ellie+ezra@>
2017-01-02 16:59:22 -0800

    works

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,14 @@
-var apiKey = "1fe9e087b202c21b52c3f82a082af452";
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
-    });
+    currentWeatherObject.getWeather(city, displayHumidity);
   });
 });
